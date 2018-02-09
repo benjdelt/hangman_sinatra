e84871ef9d721210a7c6716a0bcda17b37e85011
Benjamin Deltenre <benjdelt@gmail.com>
2018-02-09 10:59:03 +0000

    fix session bug

--- main.rb
+++ main.rb
@@ -65,43 +65,78 @@ class Hangman
 
 end
 
-
-
 get '/' do 
-  session[:hangman] = @@hangman 
-  @word = session[:hangman].word
+  @@hangman.new_game
+  session[:word] = @@hangman.word
+  session[:message] = @@hangman.message
+  session[:output] = @@hangman.output
+  session[:used_letters] = @@hangman.used_letters
+  session[:chances_left] = @@hangman.chances
+  session[:end_message] = @@hangman.end_message
+  session[:end_message_id] = @@hangman.end_message_id
+  redirect '/play'
+end 
+
+get '/play' do 
+  @word = session[:word]
   @letter = params['letter']
-  @message = session[:hangman].message
-  @output = session[:hangman].output.join(' ')
-  @used_letters = session[:hangman].used_letters.join(', ')
-  @chances_left = session[:hangman].chances
-  @end_message = session[:hangman].end_message
+  @message = session[:message]
+  @output = session[:output].join(' ')
+  @used_letters = session[:used_letters].join(', ')
+  @chances_left = session[:chances_left]
+  @end_message = session[:end_message]
   @image = "images/#{@chances_left.to_s}.jpg"
-  @end_message_id = session[:hangman].end_message_id
+  @end_message_id = session[:end_message_id]
   erb :index
   
 end 
 
-post '/' do
-  session[:hangman] = @@hangman 
-  @word = session[:hangman].word
-  @letter = params['letter']
-  @message = session[:hangman].message
-  @output = session[:hangman].output.join(' ')
-  @used_letters = session[:hangman].used_letters.join(', ')
-  @chances_left = session[:hangman].chances
-  @end_message = session[:hangman].end_message
+post '/play' do
+ 
+  @letter = params['letter'].upcase
+  #@@hangman.check_input(@letter)
+  #@@hangman.end_game?
+  
+    if @letter == "" || !session[:end_message].empty?
+     session[:message] = ""
+    elsif @letter =~ /[^A-Z]/
+      session[:message] = "Please type a letter"
+    elsif session[:used_letters].join.include?(@letter)
+      session[:message] = "You already tried that one"
+    elsif session[:word].join.include?(@letter)
+      session[:word].each_with_index do |l, i|
+        session[:output][i] = l if @letter == l 
+      end
+      session[:message] = "Right!"
+      session[:used_letters] << @letter 
+    else 
+      session[:message] = "Wrong!"
+      session[:used_letters] << @letter 
+      session[:chances_left] -= 1
+    end 
+    
+    if session[:output].join == session[:word].join
+      session[:end_message] = "Congratualtions, you won!"
+      session[:end_message_id] = "won"
+    
+    elsif session[:chances_left] <= 0
+      session[:end_message] = "Sorry, you lost. The word was \"#{session[:word].join}\""
+      session[:end_message_id]="lost"
+    end 
+    
+  @word = session[:word]
+  @message = session[:message]
+  @output = session[:output].join(' ')
+  @used_letters = session[:used_letters].join(', ')
+  @chances_left = session[:chances_left]
+  @end_message = session[:end_message]
   @image = "images/#{@chances_left.to_s}.jpg"
-  @end_message_id = session[:hangman].end_message_id
-  @@hangman.check_input(@letter)
-  @@hangman.end_game?
-  redirect '/'
+  @end_message_id = session[:end_message_id]
+  redirect '/play'
 end 
 
 get '/new_game' do
-  @@hangman.new_game
   redirect '/'
 end 
 
 @@hangman = Hangman.new 
-
